3a056c489879fab66dae3247411ae44a78a66de2
Nicolo Maioli <nicolomaioli@gmail.com>
2022-12-07 23:23:38 +0100

    Clean dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
 export { z } from "https://deno.land/x/zod@v3.19.1/mod.ts";
 export { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-export { config } from "https://deno.land/std@0.167.0/dotenv/mod.ts";
