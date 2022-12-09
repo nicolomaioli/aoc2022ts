36f06b1a5ddff5c9ca0c5efe3dd35be6e261239c
Nicolo Maioli <nicolomaioli@gmail.com>
2022-12-09 22:07:37 +0100

    Add d9p1

--- main.ts
+++ main.ts
@@ -1,63 +1,54 @@
-import { partOne as dayOnePartOne, partTwo as dayOnePartTwo } from "./lib/1.ts";
-import { partOne as dayTwoPartOne, partTwo as dayTwoPartTwo } from "./lib/2.ts";
-import {
-  partOne as dayThreePartOne,
-  partTwo as dayThreePartTwo,
-} from "./lib/3.ts";
-import {
-  partOne as dayFourPartOne,
-  partTwo as dayFourPartTwo,
-} from "./lib/4.ts";
-import {
-  partOne as dayFivePartOne,
-  partTwo as dayFivePartTwo,
-} from "./lib/5.ts";
-import { partOne as daySixPartOne, partTwo as daySixPartTwo } from "./lib/6.ts";
-import {
-  partOne as daySevenPartOne,
-  partTwo as daySevenPartTwo,
-} from "./lib/7.ts";
-import {
-  partOne as dayEightPartOne,
-  partTwo as dayEightPartTwo,
-} from "./lib/8.ts";
+import { partOne as d1p1, partTwo as d1p2 } from "./lib/1.ts";
+import { partOne as d2p1, partTwo as d2p2 } from "./lib/2.ts";
+import { partOne as d3p1, partTwo as d3p2 } from "./lib/3.ts";
+import { partOne as d4p1, partTwo as d4p2 } from "./lib/4.ts";
+import { partOne as d5p1, partTwo as d5p2 } from "./lib/5.ts";
+import { partOne as d6p1, partTwo as d6p2 } from "./lib/6.ts";
+import { partOne as d7p1, partTwo as d7p2 } from "./lib/7.ts";
+import { partOne as d8p1, partTwo as d8p2 } from "./lib/8.ts";
+import { partOne as d9p1, partTwo as d9p2 } from "./lib/9.ts";
 
-const inputOne = await Deno.readTextFile("./data/1.txt");
+const in1 = await Deno.readTextFile("./data/1.txt");
 console.log("=========================================");
-console.log("1.1", dayOnePartOne(inputOne));
-console.log("1.2", dayOnePartTwo(inputOne));
+console.log("1.1", d1p1(in1));
+console.log("1.2", d1p2(in1));
 
-const inputTwo = await Deno.readTextFile("./data/2.txt");
+const in2 = await Deno.readTextFile("./data/2.txt");
 console.log("=========================================");
-console.log("2.1", dayTwoPartOne(inputTwo));
-console.log("2.2", dayTwoPartTwo(inputTwo));
+console.log("2.1", d2p1(in2));
+console.log("2.2", d2p2(in2));
 
-const inputThree = await Deno.readTextFile("./data/3.txt");
+const in3 = await Deno.readTextFile("./data/3.txt");
 console.log("=========================================");
-console.log("3.1", dayThreePartOne(inputThree));
-console.log("3.2", dayThreePartTwo(inputThree));
+console.log("3.1", d3p1(in3));
+console.log("3.2", d3p2(in3));
 
-const inputFour = await Deno.readTextFile("./data/4.txt");
+const in4 = await Deno.readTextFile("./data/4.txt");
 console.log("=========================================");
-console.log("4.1", dayFourPartOne(inputFour));
-console.log("4.2", dayFourPartTwo(inputFour));
+console.log("4.1", d4p1(in4));
+console.log("4.2", d4p2(in4));
 
-const inputFive = await Deno.readTextFile("./data/5.txt");
+const in5 = await Deno.readTextFile("./data/5.txt");
 console.log("=========================================");
-console.log("5.1", dayFivePartOne(inputFive));
-console.log("5.2", dayFivePartTwo(inputFive));
+console.log("5.1", d5p1(in5));
+console.log("5.2", d5p2(in5));
 
-const inputSix = await Deno.readTextFile("./data/6.txt");
+const in6 = await Deno.readTextFile("./data/6.txt");
 console.log("=========================================");
-console.log("6.1", daySixPartOne(inputSix));
-console.log("6.2", daySixPartTwo(inputSix));
+console.log("6.1", d6p1(in6));
+console.log("6.2", d6p2(in6));
 
-const inputSeven = await Deno.readTextFile("./data/7.txt");
+const in7 = await Deno.readTextFile("./data/7.txt");
 console.log("=========================================");
-console.log("7.1", daySevenPartOne(inputSeven));
-console.log("7.2", daySevenPartTwo(inputSeven));
+console.log("7.1", d7p1(in7));
+console.log("7.2", d7p2(in7));
 
-const inputEight = await Deno.readTextFile("./data/8.txt");
+const in8 = await Deno.readTextFile("./data/8.txt");
 console.log("=========================================");
-console.log("8.1", dayEightPartOne(inputEight));
-console.log("8.2", dayEightPartTwo(inputEight));
+console.log("8.1", d8p1(in8));
+console.log("8.2", d8p2(in8));
+
+const in9 = await Deno.readTextFile("./data/9.txt");
+console.log("=========================================");
+console.log("9.1", d9p1(in9));
+console.log("9.2", d9p2(in9));
